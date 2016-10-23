2489958ae1573115fc53567fa54479718d5133c8
Felipe Menezes Cardoso <felipe.computacao@gmail.com>
2016-10-23 15:02:30 -0300

    some small adjusts

--- app/controller.js
+++ app/controller.js
@@ -10,57 +10,57 @@
         var database = firebase.database();
 
         var self = this;
-        
+
         self.destinations = [];
 
         self.destination = {
             url: '',
             contentType: ''
         };
-        
+
         self.validDestination = {
-        	id: '39a2c7d8-fd0f-4ae2-b1f4-1519c79cb895',
-        	url: 'http://segurobox.32sasmbbfa.us-west-2.elasticbeanstalk.com/webhooks/callback',
-        	contentType: 'text/html',
-        	secret: '70545a77-703b-4696-85ba-70948c6006c6'
+            id: '39a2c7d8-fd0f-4ae2-b1f4-1519c79cb895',
+            url: 'http://segurobox.32sasmbbfa.us-west-2.elasticbeanstalk.com/webhooks/callback',
+            contentType: 'text/html',
+            secret: '70545a77-703b-4696-85ba-70948c6006c6'
         };
-        
+
         self.invalidDestination = {
-    		id: '56bb3e84-21c9-4fca-acbb-1bb2d4b836e0',
-        	url: 'http://segurobox.32sasmbbfa.us-west-2.elasticbeanstalk.com/webhooks/doesnotexists',
-        	contentType: 'text/html',
-        	secret: '58ca75ed-bb47-4127-89c2-d4ac5253c883'
+            id: '56bb3e84-21c9-4fca-acbb-1bb2d4b836e0',
+            url: 'http://segurobox.32sasmbbfa.us-west-2.elasticbeanstalk.com/webhooks/doesnotexists',
+            contentType: 'text/html',
+            secret: '58ca75ed-bb47-4127-89c2-d4ac5253c883'
         };
 
         self.logs = [];
-        
-        self.postDefaultMessage = function(destination) {
-        	
-        	var defaultMessage = 'default message';
-        	
-        	DataService.postMessage(destination.id, destination.contentType, defaultMessage, destination.secret, function() {
-        		var toastr = $injector.get('toastr');
+
+        self.postDefaultMessage = function (destination) {
+
+            var defaultMessage = 'default message';
+
+            DataService.postMessage(destination.id, destination.contentType, defaultMessage, destination.secret, function () {
+                var toastr = $injector.get('toastr');
                 toastr.success('Post executed. See the log entry.');
-        	});
-        	
+            });
+
         };
 
         self.getDestinations = function () {
-        	
+
             DataService.getDestinations(function (response) {
-            	
-            	self.destinations = [];
-            	
-            	for (var i = 0; i < response.data.length; i++) {
-            		
-            		if (response.data[i].id !== self.validDestination.id && response.data[i].id !== self.invalidDestination.id ) {
-            			
-            			self.destinations.push(response.data[i]);
-            			
-            		}
-            		
-            	}
-                
+
+                self.destinations = [];
+
+                for (var i = 0; i < response.data.length; i++) {
+
+                    if (response.data[i].id !== self.validDestination.id && response.data[i].id !== self.invalidDestination.id) {
+
+                        self.destinations.push(response.data[i]);
+
+                    }
+
+                }
+
             });
         };
 
@@ -82,9 +82,9 @@
                 self.getDestinations();
             });
         };
-        
+
         self.openModalPostMessage = function (destination) {
-        	
+
             var modalInstance = $uibModal.open({
                 animation: false,
                 templateUrl: 'postMessage.html',
@@ -93,12 +93,12 @@
                 backdrop: 'static',
                 size: 'md',
                 resolve: {
-                	destination: function () {
+                    destination: function () {
                         return destination;
                     }
                 }
             });
-            
+
             modalInstance.result.then(function () {
                 var toastr = $injector.get('toastr');
                 toastr.success('Post executed successfully.');
@@ -106,30 +106,20 @@
             });
         };
 
-        self.read = function () {
-
-        };
-
-        self.gravar = function () {
-
-            for (var i = 1; i <= 50; i++) {
-                database.ref('log/' + i).set({value: 'testando evento ' + i});
-            }
-        };
-
-        self.gravar2 = function () {
-            database.ref('log/2').set({value: 'testando evento 2'});
-        };
-
-
         var logRef = database.ref('log/');
+
+        self.deleteLog = function () {
+            logRef = database.ref('log/');
+            logRef.remove();
+            var logView = document.querySelector('#logView');
+
+            logView.innerHTML = '';
+        };
 
         logRef.on('child_added', function (data) {
 
             var logEntry = data.val();
             var logView = document.querySelector('#logView');
-
-            console.log(logEntry.value);
 
             logView.innerHTML = logView.innerHTML + '</br>' + logEntry.value;
         });
