b3776245f91b48f7c37a2b175d36c294c99085f1
Manoel Menezes <manoel.menezes.jr@gmail.com>
2016-10-23 14:46:23 -0300

    Including predefined destinations.

--- app/controller.js
+++ app/controller.js
@@ -10,18 +10,57 @@
         var database = firebase.database();
 
         var self = this;
+        
+        self.destinations = [];
 
         self.destination = {
             url: '',
             contentType: ''
         };
+        
+        self.validDestination = {
+        	id: '39a2c7d8-fd0f-4ae2-b1f4-1519c79cb895',
+        	url: 'http://segurobox.32sasmbbfa.us-west-2.elasticbeanstalk.com/webhooks/callback',
+        	contentType: 'text/html',
+        	secret: '70545a77-703b-4696-85ba-70948c6006c6'
+        };
+        
+        self.invalidDestination = {
+    		id: '56bb3e84-21c9-4fca-acbb-1bb2d4b836e0',
+        	url: 'http://segurobox.32sasmbbfa.us-west-2.elasticbeanstalk.com/webhooks/doesnotexists',
+        	contentType: 'text/html',
+        	secret: '58ca75ed-bb47-4127-89c2-d4ac5253c883'
+        };
 
         self.logs = [];
-
+        
+        self.postDefaultMessage = function(destination) {
+        	
+        	var defaultMessage = 'default message';
+        	
+        	DataService.postMessage(destination.id, destination.contentType, defaultMessage, destination.secret, function() {
+        		var toastr = $injector.get('toastr');
+                toastr.success('Post executed. See the log entry.');
+        	});
+        	
+        };
 
         self.getDestinations = function () {
+        	
             DataService.getDestinations(function (response) {
-                self.destinations = response.data;
+            	
+            	self.destinations = [];
+            	
+            	for (var i = 0; i < response.data.length; i++) {
+            		
+            		if (response.data[i].id !== self.validDestination.id && response.data[i].id !== self.invalidDestination.id ) {
+            			
+            			self.destinations.push(response.data[i]);
+            			
+            		}
+            		
+            	}
+                
             });
         };
 
